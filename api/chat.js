f762f38a574299fe445e81e923167539cb21ca6e
Rowan <rgitbug@greatcactus.org>
2026-08-20 21:17:16 +1000

    Matrix theme + increase token limit

--- api/chat.js
+++ api/chat.js
@@ -90,7 +90,7 @@ export default async function handler(req, res) {
   const chat = model.startChat({
     history: chatHistory,
     generationConfig: {
-      maxOutputTokens: 2048,
+      maxOutputTokens: 8192,
       temperature: 0.7,
     },
   });
